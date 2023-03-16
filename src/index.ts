6c6d0ba154009979afa6fbe6bf4dcca8a1405519
TarasKing <ivanovtaras@live.ru>
2023-03-15 23:11:28 -0700

    todoList

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,60 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid";
 
-import confetti from 'canvas-confetti';
+// console.log(uuidV4());
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+type Task = {
+  id: string;
+  title: string;
+  compleated: boolean;
+  createdAt: Date;
+};
+
+const list = document.querySelector<HTMLUListElement>("#list");
+const form = document.getElementById("new-task-form") as HTMLFormElement | null;
+const input = document.querySelector<HTMLInputElement>("#new-task-title");
+
+const tasks: Task[] = loadTasks();
+tasks.forEach(addListItem);
+
+form?.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (input?.value == "" || input?.value == null) return;
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    compleated: false,
+    createdAt: new Date(),
+  };
+
+  tasks.push(newTask);
+
+  addListItem(newTask);
+  input.value = "";
+});
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", (e) => {
+    task.compleated = checkbox.checked;
+    saveTasks();
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.compleated;
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+  saveTasks();
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const taskJSON = localStorage.getItem("TASKS");
+  if (taskJSON == null) return [];
+  return JSON.parse(taskJSON);
+}
